56864096ae08aea0c1c29480cad4f1fe816301b6
dieblume86 <drouot.remi@gmail.com>
2026-05-22 19:36:46 +0200

    [Add] UniTest for Product Price Valid Cases

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,4 +1,15 @@
-﻿using Xunit;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Controllers;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -22,5 +33,144 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         }
 
         // TODO write test methods to ensure a correct coverage of all possibilities
+        [Fact]
+        public void ProductViewModel_Price_Valid_AllCases()
+        {
+            ProductViewModel_Price_IsNotValid_Empty();
+            ProductViewModel_Price_IsNotValid_NotDecimal();
+            ProductViewModel_Price_IsNotValid_NotGreaterThanZero();
+            ProductViewModel_Price_IsValid();
+        }
+        private void ProductViewModel_Price_IsNotValid_Empty()
+        {
+            // Arrange
+            var product = new ProductViewModel
+            {
+                Id = 1,
+                Name = "ProduitTest",
+                Description = "Desc",
+                Details = "Details",
+                Price = "",
+                Stock = "10"
+            };
+
+            var context = new ValidationContext(product);
+            var results = new List<ValidationResult>();
+            var isValid = false;
+
+            // Act
+            isValid = Validator.TryValidateObject(product, context, results, validateAllProperties: true);
+
+            // Assert
+            Assert.False(isValid, "ProductViewModel should be invalid beacause Price is empty");
+            Assert.NotEmpty(results);
+            Assert.Equal("MissingPrice", results[0].ErrorMessage);
+        }
+        private void ProductViewModel_Price_IsNotValid_NotDecimal()
+        {
+            // Arrange
+            var product = new ProductViewModel
+            {
+                Id = 1,
+                Name = "ProduitTest",
+                Description = "Desc",
+                Details = "Details",
+                Price = "Test",
+                Stock = "10"
+            };
+
+            var context = new ValidationContext(product);
+            var results = new List<ValidationResult>();
+            var isValid = false;
+
+            // Act
+            isValid = Validator.TryValidateObject(product, context, results, validateAllProperties: true);
+
+            // Assert
+            Assert.False(isValid, "ProductViewModel should be invalid beacause Price is not a decimal");
+            Assert.NotEmpty(results);
+            Assert.Equal("PriceNotANumber", results[0].ErrorMessage);
+        }
+        private void ProductViewModel_Price_IsNotValid_NotGreaterThanZero()
+        {
+            // Arrange
+            var product = new ProductViewModel
+            {
+                Id = 1,
+                Name = "ProduitTest",
+                Description = "Desc",
+                Details = "Details",
+                Price = "-9.99",
+                Stock = "10"
+            };
+
+            var context = new ValidationContext(product);
+            var results = new List<ValidationResult>();
+            var isValid = false;
+
+            // Act
+            isValid = Validator.TryValidateObject(product, context, results, validateAllProperties: true);
+
+            // Assert
+            Assert.False(isValid, "ProductViewModel should be invalid beacause Price is not greater than 0");
+            Assert.NotEmpty(results);
+            Assert.Equal("PriceNotGreaterThanZero", results[0].ErrorMessage);
+        }
+        private void ProductViewModel_Price_IsValid()
+        {
+            // Arrange
+            var product = new ProductViewModel
+            {
+                Id = 1,
+                Name = "ProduitTest",
+                Description = "Desc",
+                Details = "Details",
+                Price = "9.99",
+                Stock = "10"
+            };
+
+            var context = new ValidationContext(product);
+            var results = new List<ValidationResult>();
+            var isValid = false;
+
+            // Act
+            isValid = Validator.TryValidateObject(product, context, results, validateAllProperties: true);
+
+            // Assert
+            Assert.True(isValid, "ProductViewModel should be valid");
+            Assert.Empty(results);
+        }
+
+
+
+        [Fact]
+        public void CreateProductCheckName()
+        {
+            // Arrange
+            var producViewModel = new ProductViewModel
+            {
+                Id = 6,
+                Name = "",
+                Description = "TestDescription",
+                Details = "TestDetails",
+                Price = "",
+                Stock = ""
+            };
+
+            var productService = Mock.Of<IProductService>();
+            //Mock.Get(productService).Setup(m => m.CheckProductModelErrors(producViewModel));
+
+            var languageService = Mock.Of<ILanguageService>();
+            var productController = new ProductController(productService, languageService);
+
+            // Act
+            IActionResult result = productController.Create(producViewModel);
+            //IActionResult result = productController.Create(producViewModel);
+
+            // Assert
+            Assert.Equal(1, 1);
+
+            productController.DeleteProduct(6);
+        }
     }
 }
--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,20 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
-    public class ProductViewModel
+    public class ProductViewModel : IValidatableObject
     {
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "MissingQuantity")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "MissingPrice")]
         public string Price { get; set; }
+
+        /// <summary>
+        /// Validate if Price is a number and greater than zero, and if Stock is an integer and greater than zero.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!decimal.TryParse(
+                    Price,
+                    NumberStyles.Any,
+                    CultureInfo.InvariantCulture,
+                    out decimal parsedPrice))
+            {
+                yield return new ValidationResult(
+                    "PriceNotANumber",
+                    new[] { nameof(Price) });
+            }
+            else if (parsedPrice <= 0)
+            {
+                yield return new ValidationResult(
+                    "PriceNotGreaterThanZero",
+                    new[] { nameof(Price) });
+            }
+
+            if (!int.TryParse(
+                    Stock,
+                    NumberStyles.Any,
+                    CultureInfo.InvariantCulture,
+                    out int parsedStock))
+            {
+                yield return new ValidationResult(
+                    "StockNotAnInteger",
+                    new[] { nameof(Stock) });
+            }
+            else if (parsedStock <= 0)
+            {
+                yield return new ValidationResult(
+                    "StockNotGreaterThanZero",
+                    new[] { nameof(Stock) });
+            }
+        }
     }
 }
